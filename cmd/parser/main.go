971ebf60647617c7a6fdc6360f34b01de546fabf
Iovlev Alexey <singreedarrived@gmail.com>
2020-11-03 20:17:56 +0000

    WTF?!

---
+++ cmd/parser/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"projects/parser/internal/configure"
+	"projects/parser/internal/conveyer"
+	"sync"
+)
+
+var (
+	CONFIG_PATH string
+	wg          sync.WaitGroup
+)
+
+func init() {
+	flag.StringVar(&CONFIG_PATH, "config", "configs/config.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+	config := configure.New()
+	err := config.LoadToml(CONFIG_PATH)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conv := conveyer.New("remanga.org", config.GetConveyerConfig("remanga.org"))
+	conv.Start(&wg)
+	conv.GetInputChan() <- "https://remanga.org/manga/the_beginning_after_the_end?subpath=content"
+	fmt.Println(<-conv.GetOutputChan())
+}
